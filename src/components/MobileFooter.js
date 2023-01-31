3b60c7ac9691f2b0d33c8121a51ef84f75dd1bf6
GeorgeFlood <mrgeorgeflood@gmail.com>
2023-01-31 04:52:13 +0000

    Update MobileFooter.js

--- src/components/MobileFooter.js
+++ src/components/MobileFooter.js
@@ -7,16 +7,22 @@ export default function MobileFooter() {
   return (
     <div className="bg-slate-600 mt-20 text-center rounded-md py-12">
       <img className="m-auto" src={Logo} loading="lazy" alt="logo" />
-      <nav className="list-none text-slate-50 weight-semibold text-xl my-6">
-        <li>Home</li>
-        <li className="my-6">About us</li>
-        <li>Create your plan</li>
+      <nav className="list-none text-slate-50 weight-semibold text-xl my-16 ">
+        <a href="/">
+          <li className="cursor-pointer">Home</li>
+        </a>
+        <a href="/About">
+          <li className="my-6 cursor-pointer">About us</li>
+        </a>
+        <a href="/Plan">
+          <li className="cursor-pointer">Create your plan</li>
+        </a>
       </nav>
 
       <div className="flex justify-around mx-12">
-        <img src={Facebook} alt="Facebook Icon" />
-        <img src={Instagram} alt="Instagram Icon" />
-        <img src={Twitter} alt="Twitter icon" />
+        <img className="cursor-pointer" src={Facebook} alt="Facebook Icon" />
+        <img className="cursor-pointer" src={Instagram} alt="Instagram Icon" />
+        <img className="cursor-pointer" src={Twitter} alt="Twitter icon" />
       </div>
     </div>
   );
